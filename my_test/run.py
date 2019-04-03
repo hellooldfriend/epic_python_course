315a6888d5b2545f2abaf4775bb7b499f139b81a
Samat <samat1989@mail.ru>
2019-04-03 18:07:28 +0300

    first commit

---
+++ my_test/run.py
@@ -0,0 +1,10 @@
+import random
+
+a = random.randint(1,4)
+b = int(input("Enter your number:"))
+
+if a == b:
+    print("Win")
+else:
+    print("Try again")
+
---
+++ my_test/test_run.py
@@ -0,0 +1,5 @@
+def fun(x):
+    return x ** 2 + 6
+
+if __name__ == "__main__":
+    print("fun(6)")

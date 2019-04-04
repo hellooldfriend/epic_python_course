d6465ef1c5b57a8f6f9b48608dbc227c8da4e46d
Samat <samat1989@mail.ru>
2019-04-04 13:43:39 +0300

    Updated lesson 4

--- lesson_4/str_n.py
+++ lesson_4/str_n.py
@@ -1,23 +1,10 @@
-s = "У лукоморья 123 дуб зеленый 456"
+s = input("Write something: ")
+n = int(input("Enter the number: "))
 
-def fun_one(s):
-    return s.find('я')
+def fun(s, n):
+    if len(s) > n:
+        return s.upper()
+    else:
+        return s
 
-def fun_two(s):
-    return s.count('у')
-
-def fun_three(s):
-    # swapcase also works here
-    return s.upper()
-
-def fun_four(s):
-    # swapcase also works here
-    if len(s) > 4:
-        return s.lower()
-
-def fun_five(s):
-    return s.replace( s[0], 'О')
-
-
-
-
+print(fun(s, n))

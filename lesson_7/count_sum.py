da409a84fa70d3d876cd466b2c5e2add0ee892e9
Samat <samat1989@mail.ru>
2019-05-03 20:54:53 +0300

    Updated homework

--- lesson_7/count_sum.py
+++ lesson_7/count_sum.py
@@ -1,12 +1,19 @@
 #Дано число, введенное с клавиатуры.
 #Определить сумму квадратов нечетных цифр в числе.
 
-val = int(input('Enter the number: '))
-total = 0
+val = input('Enter the number: ')
 
-for i in range(val + 1):
-    if i % 2 != 0:
-        n = i ** 2
-        total += n
+def f(n):
+    summ = 0
 
-print(total)
+    for i in n:
+        if int(i) % 2 != 0:
+            summ += int(i) ** 2
+
+    if summ == 0:
+        return 'The number does not have odd numbers'
+    return summ
+
+
+###
+print(f(val))

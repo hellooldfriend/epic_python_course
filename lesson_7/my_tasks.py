499231a1b35627fec7175103d9a9d6cdf7d8aad3
Samat <samat1989@mail.ru>
2019-05-06 17:13:21 +0300

    updated my_tasks.py and random1000.py

--- lesson_7/my_tasks.py
+++ lesson_7/my_tasks.py
@@ -3,4 +3,27 @@
 #Добавить возможность вывода списка задач.
 
 
-# Сделал в задании 9.2 с помошью dict. Без dict запарно получается.
+tasks = ['description', 'category', 'date']
+
+while True:
+    inp = int(input('Enter the number: '))
+
+    if inp == 1:
+        desc = input('Add a description: ')
+        tasks.append(desc)
+        cat = input('Add a category: ')
+        tasks.append(cat)
+        date = input('Add a date: ')
+        tasks.append(date)
+
+    if inp == 2:
+        tasks = tasks[3:]
+        index = 0
+
+        for i in range(int(len(tasks) / 3)):
+            print(f"Task: {tasks[index]}, category: {tasks[index + 1]}, date: {tasks[index + 2]}")
+            index += 3
+
+    if inp == 3:
+        break
+
--- lesson_9/random1000.py
+++ lesson_9/random1000.py
@@ -7,11 +7,19 @@ import random
 numbers = [random.randint(-500, 500) for i in range(1001)]
 
 def f(lst):
+    max_index = lst.index(max(lst))
+    min_index = lst.index(min(lst))
+
+    lst = lst[min_index:]
+    lst = lst[:max_index]
     count = 0
-    for n in lst:
-        if n < 0:
-            count += 1
+
+    for x in lst:
+        if x < 0:
+            count +=1
     return count
+
+
 
 
 min_values_count = f(numbers)

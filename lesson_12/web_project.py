266c6ea346250a74c900872a12e139b555552a44
Samat <samat1989@mail.ru>
2019-05-16 17:11:50 +0300

    updated hw

--- lesson_12/web_project.py
+++ lesson_12/web_project.py
@@ -3,40 +3,36 @@ import sqlite3
 
 app = Flask(__name__)
 
+file_address = '../lesson_11/tasks.db'
 
-
-@app.route('/show_tasks/', methods=['GET'])
-def show_tasks():
-    conn = sqlite3.connect('../lesson_11/tasks.db')
+def read():
+    conn = sqlite3.connect(file_address)
     cursor = conn.cursor()
     result = cursor.execute('select * from tasks')
     data = result.fetchall()
-    return render_template('index.html', name='show task worked', data=data)
+    return data
+
+@app.route('/show_tasks/', methods=['GET'])
+def show_tasks():
+    data = read()
+    return render_template('index.html', name='Shows task', data=data)
 
 
 @app.route('/')
 def index():
-    test = 'Works??'
-    return render_template('index.html', name=test)
-
+    title = 'I\'m your task manager, you may add a task.'
+    return render_template('index.html', name=title)
 
 
 @app.route('/', methods=['POST'])
 def add_task():
-    conn = sqlite3.connect('../lesson_11/tasks.db')
+    conn = sqlite3.connect(file_address)
     cursor = conn.cursor()
-
-    # taskname = request.form['name']
-    # category = request.form['category']
-    # datetime = request.form['datetime']
-
-    # print(taskname, category, datetime)
 
     cursor.execute("insert into tasks (id, category, name, time) values (NULL, ?, ?, ?)",
                     (request.form['name'], request.form['category'], request.form['datetime']))
     conn.commit()
     conn.close()
-    return show_tasks()
-    # return render_template('index.html', name='add_task worked')
+    return render_template('index.html', name='Task added')
 
 

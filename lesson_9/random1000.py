9233c1198d73923c28da8d6d11a03e4cff77790c
Samat <samat1989@mail.ru>
2019-05-17 18:19:27 +0300

    update hw

--- lesson_9/random1000.py
+++ lesson_9/random1000.py
@@ -7,11 +7,17 @@ import random
 numbers = [random.randint(-500, 500) for i in range(1001)]
 
 def f(lst):
+    '''
+    >>> f([-1, 25, -100, 4, 7, -2, -9, -25, 200, -7, 30])
+    4
+    >>> f([100, -1, -2, -200, -4, -2, 2, 4, 1000, -4, 3])
+    3
+    '''
     max_index = lst.index(max(lst))
     min_index = lst.index(min(lst))
 
-    lst = lst[min_index:]
-    lst = lst[:max_index]
+
+    lst = lst[min_index:max_index]
     count = 0
 
     for x in lst:
@@ -21,9 +27,6 @@ def f(lst):
 
 
 
-
-min_values_count = f(numbers)
-
-print(min_values_count)
-
+if __name__ == '__main__':
+    print(f(numbers))
 
